4738ec37721acfb02a41e77ab709aaedfc0d31f0
junnnnnnnyoung <128367271+junyoung93@users.noreply.github.com>
2023-06-19 15:14:04 +0900

    Add files via upload

---
+++ ch7_8.java
@@ -0,0 +1,14 @@
+package chapter7;
+
+public class ch7_8 {
+
+    //7-8
+    // 접근제어자 접근범위가 넓은 것에서 좁은 것의 순 나열
+    // public -> protected -> default -> private   protected는 같은 패키지네 자손클래스도 접근이 가능해서
+
+    //7-9
+    // 제어자 final은 ''
+
+
+
+}
