9dcb71b92244a61677d53011f58390b24a83b052
PARKJUNYOUNG\USER <yyijs0309@gmail.com>
2023-06-21 23:02:13 +0900

    230621공부 추가

--- untitled/src/Solution/Solution.java
+++ untitled/src/Solution/Solution.java
@@ -1,26 +1,45 @@
 package Solution;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 class Solution {
-    public int solution(int[][] sizes) {
-        int answer = 0;
+    public int[] solution(int[] answers) {
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int w = 0;
-        int h = 0;
+        int[] cnt = new int[3];
 
-        for (int i = 0; i < sizes.length; i++) {
-            Arrays.sort(sizes[i]);
-
-            for (int j = 0; j < 2; j++) {
-                if( w < sizes[i][0]){
-                    w = sizes[i][0];
-                }
-            }   if( h < sizes[i][1]){
-                h = sizes[i][1];
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == person1[i % 5]) {
+                cnt[0]++;
+            }
+            if (answers[i] == person2[i % 8]) {
+                cnt[1]++;
+            }
+            if (answers[i] == person3[i % 10]) {
+                cnt[2]++;
             }
         }
-        return w*h;
+
+        int max = Math.max(Math.max(cnt[0], cnt[1]), cnt[2]);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if (max == cnt[0]) {
+            list.add(1);
+        }
+        if (max == cnt[1]) {
+            list.add(2);
+        }
+        if (max == cnt[2]) {
+            list.add(3);
+        }
+
+        int[] answer = new int[list.size()];
+        for (int j = 0; j < list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+
+        return answer;
     }
 }
