1f57eb8dc4c1977de81e83f58f62cf2790f40f9d
PARKJUNYOUNG\USER <yyijs0309@gmail.com>
2023-07-07 21:19:11 +0900

    2300707공부 추가

---
+++ untitled/src/day230707/day230707.java
@@ -0,0 +1,21 @@
+package day230707;
+
+public class day230707 {
+//    아래의 괄호안에 알맞은 함수형 인터페이스는?
+//            (     ) f :
+//            f = (int a, int b) -> a > b ? a : b;
+//
+//    a. Function
+//    b. BiFunction
+//    c. Predicate
+//    d. IntBinaryOperator
+//    e. IntFunction
+
+
+}
+
+// a  c  e는 하나의 매개변수 받음
+// BiConsumer , BiPredicate , BiFunction 은 두개의 매개변수를 받는다.
+//BiConsumer 는 반환값 x
+// BiPredicate 는 조건식을 표현하는데 사용 // 반환값은 boolean
+// BiFunction int를 반환 x 기본형 x
